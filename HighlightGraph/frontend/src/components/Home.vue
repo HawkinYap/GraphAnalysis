ff465d101a63227ce13c1538f2060ff17464eee9
cnyangkui <1371826591@qq.com>
2019-08-26 17:24:30 +0800

    add Test, Intro

--- HighlightGraph/frontend/src/components/Home.vue
+++ HighlightGraph/frontend/src/components/Home.vue
@@ -5,25 +5,32 @@
       <div class="mutiple-progress-container">
         <div class="mutiple-progress">
           <div class="circle-container">
-            <div class="circle"></div>
-            <p class="number">1</p>
+            <div class="circle" @click="toIntro();"></div>
+            <p class="number" @click="toIntro();">1</p>
           </div>
           <div class="progress">
-            <el-progress :show-text="false" :stroke-width="20" :percentage="100" color="#ccc"></el-progress>
+            <el-progress :show-text="false" :stroke-width="20" :percentage="intro_percentage" color="#ccc"></el-progress>
           </div>
           <div class="circle-container">
-            <div class="circle"></div>
-            <p class="number">2</p>
+            <div class="circle" @click="toTest();"></div>
+            <p class="number" @click="toTest();">2</p>
           </div>
           <div class="progress">
-            <el-progress :show-text="false" :stroke-width="20" :percentage="0"></el-progress>
+            <el-progress :show-text="false" :stroke-width="20" :percentage="test_percentage" color="#ccc"></el-progress>
           </div>
           <div class="circle-container">
             <div class="circle"></div>
             <p class="number">3</p>
+            <div>
+                <select>
+                    <option>A</option>
+                    <option>B</option>
+                    <option>C</option>
+                </select>
+            </div>
           </div>
           <div class="progress">
-            <el-progress :show-text="false" :stroke-width="20" :percentage="0"></el-progress>
+            <el-progress :show-text="false" :stroke-width="20" :percentage="experiment_percentage" color="#ccc"></el-progress>
           </div>
           <div class="circle-container">
             <div class="circle"></div>
@@ -39,20 +46,43 @@
 import * as d3 from "d3"
 import axios from '../assets/js/http'
 export default {
-  name: 'NodeLinkGraph',
+  name: 'Home',
   props: {
   },
   data() {
     return {
-      
+			intro_percentage: 0,
+			test_percentage: 0,
+			experiment_percentage: 0,
     }
   },
   mounted() {
     this.$nextTick(() => {
-      
+			let msg = this.$route.params.msg;
+			switch (msg) {
+				case 'intro':
+					this.intro_percentage = 100;
+					break;
+				case 'test':
+					this.intro_percentage = 100;
+					this.test_percentage = 100;
+					break;
+				default:
+					break;
+			}
     })
   },
   methods: {
+    toIntro() {
+			this.$router.push('/intro');
+		},
+		toTest() {
+			if(this.intro_percentage == 0) {
+				alert("请按序操作...")
+				return;
+			}
+			this.$router.push('/test');
+		}
   },
   computed: {
   }
@@ -72,7 +102,7 @@ export default {
   margin-top: 15%;
 }
 #processing {
-  font-size: 30px;
+  font-size: 40px;
   font-weight: bold;
   color: #ccc;
 }
@@ -106,11 +136,13 @@ export default {
   height: 100%;
   background-color: #ccc;
   border-radius: 50%;
+  cursor: pointer;
 }
 .number {
   font-size: 25px;
   color: #fff;
   line-height: 50px;
   margin-top: -50px;
+  cursor: pointer;
 }
 </style>
---
+++ HighlightGraph/frontend/src/components/Introduction.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <input type="button" name="" id="" @click="toHome();">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+  },
+  data() {
+    return {
+      
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      
+    })
+  },
+  methods: {
+    toHome() {
+      this.$router.push({ name: 'home', params: { msg: 'intro' }});
+    }
+  },
+  computed: {
+  }
+}
+</script>
