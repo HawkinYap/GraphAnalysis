f44547e042be3089be6cb205411947862ffe9fe5
cnyangkui <1371826591@qq.com>
2019-09-03 20:28:33 +0800

    django admin

--- HighlightGraph/backend/admin.py
+++ HighlightGraph/backend/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from backend import models
 
 # Register your models here.
+
+class UsernameAdmin(admin.ModelAdmin):
+    list_display = ('username','sex', 'age', 'education', 'research')
+
+class DurationAdmin(admin.ModelAdmin):
+    list_display = ('did', 'time', 'name', 'consumingtime', 'username')
+
+class RectangleAdmin(admin.ModelAdmin):
+    list_display = ('rid', 'time', 'name', 'x1', 'y1', 'x2', 'y2', 'username', 'duration')
+
+admin.site.register(models.Username, UsernameAdmin)
+admin.site.register(models.Duration, DurationAdmin)
+admin.site.register(models.Rectangle, RectangleAdmin)
