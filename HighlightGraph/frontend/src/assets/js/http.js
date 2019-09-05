ae0ea0927bac7825f254af82529593adf99f27c2
cnyangkui <1371826591@qq.com>
2019-09-05 10:45:12 +0800

    多主机请求测试

--- HighlightGraph/frontend/src/assets/js/http.js
+++ HighlightGraph/frontend/src/assets/js/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000';
+axios.defaults.baseURL = 'http://192.168.0.122:8000';
 axios.defaults.withCredentials = true;
 axios.defaults.headers.post['Content-Type'] = 'application/json' //'application/x-www-form-urlencoded';
 
