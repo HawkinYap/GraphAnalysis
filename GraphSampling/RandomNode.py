b10a1791fb7a2343f0764b7c61b141eff71c9903
HawkinYap <Gallowsgogo@gmail.com>
2019-09-12 16:33:01 +0800

    修改随机节点采样

--- GraphSampling/RandomNode.py
+++ GraphSampling/RandomNode.py
@@ -1,31 +1,14 @@
 import random
 import networkx as nx
+from random import choice
 
 class RandomNode():
-
-    def __init__(self):
-        self.G1 = nx.Graph()
-
     def randomnode(self, G, size):
         list_nodes = list(G.nodes())
+        select_nodes = random.sample(list_nodes, size)
+        induced_graph = G.subgraph(select_nodes)
+        return induced_graph
 
-        nr_nodes = len(list_nodes)
-        upper_bound_nr_nodes_to_sample = size
 
-        sampled_graph = []
-        sampled_graph_edges = []
-
-        while (len(sampled_graph) < upper_bound_nr_nodes_to_sample):
-            index_of_first_random_node = random.randint(0, nr_nodes - 1)
-            if list_nodes[index_of_first_random_node] in sampled_graph:
-                continue
-            else:
-                sampled_graph.append(list_nodes[index_of_first_random_node])
-                tmpnodes = [n for n in G.neighbors(list_nodes[index_of_first_random_node])]
-                for tmpnode in tmpnodes:
-                    sampled_graph_edges.append([list_nodes[index_of_first_random_node], tmpnode])
-
-        self.G1.add_edges_from(sampled_graph_edges)
-        return self.G1
 
 
--- test_sample2.py
+++ test_sample2.py
@@ -5,64 +5,87 @@ import csv
 import pandas as pd
 
 # load graph to networkx
-f = open("Datasets/relationship.csv", "r")
-reader1 = csv.reader(f)
-edges = []
-for item in reader1:
-    edges.append([int(item[0]), int(item[1])])
-f.close()
-G = nx.Graph()
-G.add_edges_from(edges)
+def loadData(path, isDirect):
 
-# set sampling rate
-total = len(G.nodes())
-rate = 0.5
-sample_rate = int(total * rate)
+    f = open(path, "r")
+    reader1 = csv.reader(f)
+    edges = []
+
+    for item in reader1:
+        edges.append([int(item[0]), int(item[1])])
+    f.close()
+    if isDirect:
+        G = nx.DiGraph()
+    else:
+        G = nx.Graph()
+    G.add_edges_from(edges)
+    return(G)
 
 # graph sampling
-FF_object = GraphSampling.ForestFire()
-FF_sample = FF_object.forestfire(G, sample_rate)  # graph, number of nodes to sample
-print(len(FF_sample.nodes()))
+def graphSampling(G):
+
+    # set sampling rate
+    total = len(G.nodes())
+    rate = 0.5
+    sample_rate = int(total * rate)
+
+    RN_object = GraphSampling.RandomNode()
+    RN_sample = RN_object.randomnode(G, sample_rate)  # graph, number of nodes to sample
+    return(RN_sample)
 
 
-# origin graph
-plt.subplot(221)
-spring_pos = nx.spring_layout(G)
-plt.title('original graph')
-nx.draw(G, spring_pos, with_labels=True)
+def drawGraph(G, sample):
+    # origin graph
+    plt.subplot(221)
+    spring_pos = nx.spring_layout(G)
+    plt.title('original graph')
+    nx.draw(G, spring_pos, with_labels=True)
 
-plt.subplot(222)
-plt.title('sampling graph')
-nx.draw(FF_sample, spring_pos, with_labels=True)
-plt.show()
-
-
-# convert to node list
-class_nodes = []
-for node in G.nodes():
-    if node in FF_sample.nodes():
-        class_nodes.append([node, 2])
-    else:
-        class_nodes.append([node, 1])
-
-# convert to edge list
-orig_edges = []
-for edge in G.edges():
-    if edge in FF_sample.edges():
-        orig_edges.append([edge[0], edge[1], 2])
-    else:
-        orig_edges.append([edge[0], edge[1], 1])
-
-# test csv
-classfile_path = 'SamplingDatasets/RN_RS_node.csv'
-orig_edgefile_path = 'SamplingDatasets/RN_RS_edge.csv'
-
-title = ['ID', 'Class']
-test = pd.DataFrame(columns=title, data=class_nodes)
-test.to_csv(classfile_path)
-
-title = ['Source', 'Target', 'Type']
-test = pd.DataFrame(columns=title, data=orig_edges)
-test.to_csv(orig_edgefile_path)
+    plt.subplot(222)
+    plt.title('sampling graph')
+    nx.draw(sample, spring_pos, with_labels=True)
+    plt.show()
 
 
+def saveGraph(G, sample):
+
+    # convert to node list
+    class_nodes = []
+    for node in G.nodes():
+        if node in sample.nodes():
+            class_nodes.append([node, 2])
+        else:
+            class_nodes.append([node, 1])
+
+    # convert to edge list
+    orig_edges = []
+    for edge in G.edges():
+        if edge in sample.edges():
+            orig_edges.append([edge[0], edge[1], 2])
+        else:
+            orig_edges.append([edge[0], edge[1], 1])
+
+    # test csv
+    classfile_path = 'SamplingDatasets/RN_RS_node.csv'
+    orig_edgefile_path = 'SamplingDatasets/RN_RS_edge.csv'
+
+    title = ['ID', 'Class']
+    test = pd.DataFrame(columns=title, data=class_nodes)
+    test.to_csv(classfile_path, index=None)
+
+    title = ['Source', 'Target', 'Type']
+    test = pd.DataFrame(columns=title, data=orig_edges)
+    test.to_csv(orig_edgefile_path, index=None)
+
+def dataTest():
+    path = "Datasets/relationship.csv"
+    isDirect = False
+    G = loadData(path, isDirect)
+    sample = graphSampling(G)
+    drawGraph(G, sample)
+    saveGraph(G, sample)
+
+
+
+if __name__ == '__main__':
+    dataTest()
