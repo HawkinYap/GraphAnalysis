0dce770f96586374dabd0c8f68a90e7dd1f36d81
HawkinYap <Gallowsgogo@gmail.com>
2019-11-26 22:14:54 +0800

    添加实验数据

---
+++ OtherAlgorithm/connect.py
@@ -0,0 +1,54 @@
+import networkx as nx
+import csv
+import os
+
+
+# load graph to networkx
+def loadData(path1, path2, isDirect):
+
+    # add nodes
+    f = open(path1, "r")
+    reader1 = csv.reader(f)
+    nodes = []
+    for item in reader1:
+        nodes.append(int(item[0]))
+    f.close()
+    if isDirect:
+        G = nx.DiGraph()
+    else:
+        G = nx.Graph()
+    G.add_nodes_from(nodes)
+
+    # add edges
+    f = open(path2, "r")
+    reader1 = csv.reader(f)
+    edges = []
+    for item in reader1:
+        edges.append([int(item[0]), int(item[1])])
+    f.close()
+    G.add_edges_from(edges)
+
+    # add edge attribution
+    i = 0
+    for u, v, d in G.edges(data=True):
+        G[u][v]['weight'] = 1
+        i += 1
+    return (G)
+
+
+def dataTest():
+    path1 = "Data/toy1_node.csv"
+    path2 = "Data/toy1_edge.csv"
+
+    file = os.path.splitext(path1)
+    filename, type = file
+    a = filename.split('/')
+    b = a[-1].split('_')
+    fn = b[0]
+
+    isDirect = False
+    G = loadData(path1, path2, isDirect)
+    print(list(nx.connected_components(G)))
+
+if __name__ == '__main__':
+    dataTest()
